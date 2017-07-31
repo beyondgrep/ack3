8b6fb265795f3a494d77ac11a937ffb9b2b42851
Bill Ricker aka N1VUX <bill.n1vux@gmail.com>
2017-07-30 23:51:27 -0400

    added comments from #26 to Cookbook.pm POD and organized

--- lib/App/Ack/Docs/Cookbook.pm
+++ lib/App/Ack/Docs/Cookbook.pm
@@ -13,7 +13,9 @@ Note: on Windows CMD prompt, the quoting may need to switch C<"> for C<'> and/or
 
 =head1 COMPOUND QUERIES
 
-=head2 Find "goat(s)" or "cow(s(" or both
+Some compound queries can be done in a single RE pattern but most will require a pipeline command.
+
+=head2 Find "goat(s)" or "cow(s)" or both
 
     ack '(goats?|cows?)'
 
@@ -34,7 +36,7 @@ having to get F<xargs> involved.
     ack -l cows | ack -x goats
 
 
-=head2 find goats in files that do not contain cows
+=head2 Find goats in files that do not contain cows
 
     ack -L cows | ack -x -l goats
 
@@ -64,7 +66,7 @@ But for just two, we can easily do it in on go, too:
 
 C<|> says "OR".
 
-C<< \b >> says word boundary here, letters one side and space or puctuation on the other,
+C<< \b >> says word boundary here, letters one side and space or punctuation on the other,
 so half of a Whole-words limitation.
 
 C<.*> says anything or nothing, but wrapped in C<\b.*\b> with words on outsides,
@@ -82,6 +84,19 @@ We could have typed C<< --no-smart-case >> but that's too much to type,
 so C<-I> on commandline, longform in scripts!)
 
 
+=head2 Find all goat(s) in files without cows
+
+ Search all the files that don't have cow(s) and show their goat(s).
+
+    ack -L 'cows?' | ack -x -w 'goats?'
+
+=head2  Search and highlight a specific pattern 'goat' but exclude lines that match also another pattern 'cow'.
+
+    ack '^(?!.*?cow).*?\Kgoat'
+
+The C<\K> is called KEEP; aside from preventing backtracking, it resets the C<$` $&> boundary, the start of match.
+C<(?!.*cow)> is a negative lookahead.
+C<.*?> is non-greedy so the search is left to right.
 
 
 
@@ -128,7 +143,7 @@ The first ack finds only the lines in the Apache log for the given
 IP.  The second finds the match on my troublesome GIF, and shows
 the previous five lines from the log in each case.
 
-=head2 use ack instead of find
+=head2 Use ack instead of find
 
     ack -f --html --print0 | xargs -0 wc -l
 
@@ -145,10 +160,12 @@ or
 (Optionally followed by "word boundary" marker C<\b>
 as in  C<< [-]>method\b >> to not find C<< ->methodically >> .)
 
-=head2 use C<-w> only for words
+=head2 Use C<-w> only for words
 
-C<ack -w pattern> will restrict the match to C<pattern> as a word, surrounded by whitespace or puntuation
+C<ack -w pattern> will restrict the match to C<pattern> as a word, surrounded by whitespace or punctuation
 (and both ends of lines count as whitespace).
+(Word means alphabetic or '_' in this context.
+If your OS tells Perl that certain accented characters are alphabetic for you, they may be included, try it!)
 
 If your desired pattern begins with one word and ends with another, C<-w> is still safe.
 
@@ -156,14 +173,24 @@ If your desired pattern starts or ends (or both) with punctuation, using C<-w> m
 (and will likely warn so).
 In which case, SWYM: Say What You Mean.
 If you mean the start of your pattern with punctuation should be
-at the begining of line B<OR> preceded by a word OR by a space (or tab etc), before the needed punc,
+at the beginning of line B<OR> preceded by a word OR by a space (or tab etc), before the needed punctuation,
 SWIM as C<(?:^|\b|\s)#+\s+(.*)>
 which will find and capture a comment.
 (Try it plain, with C<-o>, and with C<--output='$1'>, which has its own section below)
 
-So don't look for C<ack -w '$identier'>, it won't match C<+$identifier> or C<func($identifier)> and won't even find it in column 1; do instead
+So don't look for C<ack -w '$identifier'>, it won't match C<+$identifier> or C<func($identifier)> and won't even find it in column 1; do instead
 
    ack '(?:^|\s|\b)\$identifier\b'
+
+   ack '(?x)(?: ^ | \b | (?<=  \W | \s  )) [#]  DEBUG (?= \b | \s | $) '
+
+SWYMingly finds C<< #DEBUG >> if at beginning of line, after a word break, a non-word char, or a space char,
+and followed by a word-break, or space, or end-of-line. 
+This uses look-behind and look-ahead so that only B<#DEBUG> is highlighted (or saved to C<$&>),
+and also C<(?x)> the B<extended> syntax, in which whitespace is only match if explicitly C<\s> or C<\0x20> or C<[ ]>,
+the blanks are for readability.
+
+(This is over-specified, since C<G> followed by space would be C<\b>, and C<\s> is C<\W>, but if you're not sure, it's OK to over-specify ! )
 
 
 =head2 See all the C<.vim> files in your hidden C<~/.vim> directory, except in the C<bundle/> directory.
@@ -180,7 +207,7 @@ Open all the files that have taxes in them.
 
 =head2 Find all the Perl test files and test them
 
-Use C<ack>'s filetype inference to find things of C<--type=perltest>
+Use C<ack>'s file-type inference to find things of C<--type=perltest>
 and feed them to C<xargs> which runs them all against the C<prove> command.
 
      ack -f --perltest | xargs prove
@@ -212,13 +239,14 @@ And use -H to show what line numbers are.
 
     ack -L 'warnings?_' $(ack -l Test::Warn)
 
-=head2 TBD these are notes to be fleshed out TBD
 
-=head2 TBD Mention dirty and/or git diff --name-only.
+=head2 Search only recently changed or 'dirty' files
 
-=head2 TBD  Search all the files that don't have foo and show their usage of bar. Think of a better example.
+Most version control systems provide a query to list files changed since checkout, often referred to as 'dirty' files. E.g.,
 
-    ack -L foo | ack -x -w bar
+   alias dirty="git diff --name-only"
+
+   dirty | ack -x pattern
 
 =head1 EXAMPLES OF C<< --output >>
 
@@ -275,7 +303,21 @@ This shows how to pick out particular parts of a match using ( ) within regular 
      ack '^sub (\w+)' --perltest --output='$1' -h --nogroup | sort | uniq -c  | sort -n
 
 
+=head2 In COBOL source code, match only lines with blank in column 7, ignore
 
+    ack '^.{6}[ ].*?\Kpattern'
+    ack '(?x) ^ .{6} [ ] .*? \K pattern'  # same but readable
+
+    
+Again using the C<\K> Keep to reset start of matching.
+    
+(Legacy COBOL put C<'*'> in Col 7 for comments, back in punch-card days. 
+FORTRAN in the day similarly used 'C' or '*' in Col 1. 
+Early FORTRAN wrapped lines with C<&> in Col 73 and in Col 6 of next line.) 
+
+
+(Hat-tip for Question to Pierre)
+   
 
 
 =head1 VERY ELEGANT ACK
@@ -324,7 +366,7 @@ A user who really wants the working directory reported makes a C<bash function> 
 (I can't really recommend C<chmod 777> , you're better off with per-user temp sub-dirs for security safety,
 whether under C<$HOME> or under C</tmp/$USER>.)
 
-=head2 find log lines with 4 nulls and sort by IP address
+=head2 Find log lines with 4 nulls and sort by IP address
 
 
 via L<@clmagic|https://twitter.com/clmagic> "Command Line Magic" twitter bot -
@@ -345,7 +387,7 @@ larger than even C<egrep>'s, to better SWYM DRY (Don't Repeat Yourself):
     ack -- "(?x: \t  (?: - \t ){4} )" entries.txt |sort -k3V
 
 to explicitly count (C<< (?:  ){4} >>) the tab-separated dashes.
-The C<< (?x: ) >>  says spaces don't count, are used for readibility.
+The C<< (?x: ) >>  says spaces don't count, are used for readability.
 If 'null' was optional spaces between the tabs not a single dash,
 we'd use a character class of just space C<< [ ] >>:
 
@@ -357,7 +399,7 @@ Is that one space or two there?)
 (L<regex cheatsheat comparing ack's perlRe with (e)grep, sed, ...|https://remram44.github.io/regex-cheatsheet/regex.html>
 
 
-=head2 Summarize the filetypes in your project
+=head2 Summarize the file-types in your project
 
     $ ack --noenv --show-type -f | perl -MData::Dumper -naE'++$n{$F[-1]}; END {print Dumper \%n}'
     $VAR1 = {
@@ -380,7 +422,7 @@ Is that one space or two there?)
 In old C<ack2>, Mark Fowler demonstrated the reason we banned C<--output>
 from the new Project scoped C<.ackrc> in the PerlAdvent calendar i
 L<http://www.perladvent.org/2014/2014-12-21.html>,
-to annotate the URLs found in a file with their download weighti:
+to annotate the URLs found in a file with their download weights:
 
     ack2 --output='$&: @{[ eval "use LWP::Simple; 1" && length LWP::Simple::get($&) ]} bytes' \
            'https?://\S+' list.txt
@@ -401,6 +443,21 @@ But you can sill do this, it just requires a pipe --
 
 =head2 KWIC: KeyWord in Context index
 
+A KeyWord In Context (KWIC) index was more useful in the days of offline computing and line-printer reports but is still sometimes relevant to see the matches not (just) highlighted but lined up for easy scanning.
+
+The tradidional distinction between KWIC and KWOC is whether the Keyword is at start of line with it's left context wrapped (KWOC= Out of), 
+or tab separated in the middle. KWIC works best with two word-processor fixed tabsets, not with 8-char tabs, alas.
+
+            ack  --output '$&^I$'"'"'^I|| $`' pattern files | sort     # KWOC
+            ack  --output '$&^I$\'^I|| $`'    pattern files | sort     # KWOC
+
+            ack  --output '$`^I$&^I$'"'" pattern files | sort -df -t^I -k F2,F2 # pseudo KWIC 
+            ack  --output '$`^I$&^I$\''  pattern files | sort -df -t^I -k F2,F2 # pseudo KWIC 
+
+(On the KWOC, the C<||> shows where right and left margin are wrapped.)
+(To make the KWIC output look right, load into OpenOffice or Word to spread the tab stops !)
+
+ 
 =head2 TBD lookahead and lookbehind
 
 =cut;
