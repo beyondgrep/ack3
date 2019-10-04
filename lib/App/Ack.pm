b3c43d44109dea6ebc0753107a8e85a6b322f4ca
Andy Lester <andy@petdance.com>
2019-10-03 20:32:26 -0500

    No longer uses version.pm

--- lib/App/Ack.pm
+++ lib/App/Ack.pm
@@ -16,8 +16,7 @@ A container for functions for the ack program.
 our $VERSION;
 our $COPYRIGHT;
 BEGIN {
-    use version;
-    $VERSION = version->declare( 'v3.1.1' ); # Check https://beyondgrep.com/ for updates
+    $VERSION = v3.1.1; # Check https://beyondgrep.com/ for updates
     $COPYRIGHT = 'Copyright 2005-2019 Andy Lester.';
 }
 our $STANDALONE = 0;
