ef2e551db91bf79e35826ce7579c8045fc8fc765
Andy Lester <andy@petdance.com>
2023-02-25 14:26:59 -0600

    Releasing 3.7.0

--- lib/App/Ack.pm
+++ lib/App/Ack.pm
@@ -16,8 +16,8 @@ A container for functions for the ack program.
 our $VERSION;
 our $COPYRIGHT;
 BEGIN {
-    $VERSION = 'v3.6.0'; # Check https://beyondgrep.com/ for updates
-    $COPYRIGHT = 'Copyright 2005-2022 Andy Lester.';
+    $VERSION = 'v3.7.0'; # Check https://beyondgrep.com/ for updates
+    $COPYRIGHT = 'Copyright 2005-2023 Andy Lester.';
 }
 our $STANDALONE = 0;
 our $ORIGINAL_PROGRAM_NAME;
