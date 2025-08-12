ae25a78b0b16394dfdf817aa5a319e4b81e51e4b
Dmitri Vereshchagin <dmitri.vereshchagin@gmail.com>
2025-08-12 19:25:18 +0300

    Fix inclusion of excluded type with multiple filters

--- lib/App/Ack/ConfigLoader.pm
+++ lib/App/Ack/ConfigLoader.pm
@@ -124,8 +124,9 @@ sub _uninvert_filter {
     for ( my $i = 0; $i < @{ $opt->{filters} }; $i++ ) {
         my $opt_filter = @{ $opt->{filters} }[$i];
 
-        # XXX Do a real list comparison? This just checks string equivalence.
-        if ( $opt_filter->is_inverted() && "$opt_filter->{filter}" eq "@filters" ) {
+        if ( $opt_filter->is_inverted()
+            && grep { $_ == $opt_filter->{filter} } @filters )
+        {
             splice @{ $opt->{filters} }, $i, 1;
             $i--;
         }
