e627c18e77d635b0dad69c77749e9296071b8389
Andy Lester <andy@petdance.com>
2026-06-07 11:51:35 -0500

    Bump up to v3.10.0 for release

--- lib/App/Ack.pm
+++ lib/App/Ack.pm
@@ -18,7 +18,7 @@ no re 'eval'; # enforce default, block -Mre=eval to block RCE security fail
 our $VERSION;
 our $COPYRIGHT;
 BEGIN {
-    $VERSION = 'v3.9.0'; # Check https://beyondgrep.com/ for updates
+    $VERSION = 'v3.10.0'; # Check https://beyondgrep.com/ for updates
     $COPYRIGHT = 'Copyright 2005-2026 Andy Lester.';
 }
 our $STANDALONE = 0;
